f5dd03070a5d8a70038094d1f3cc9c7e3b22cd26
Fernando Kabas <fkabas@binit.dev>
2024-04-28 20:34:55 +0200

    Add try catch clause on endpoint

--- WebAPI/Controllers/JourneyController.cs
+++ WebAPI/Controllers/JourneyController.cs
@@ -25,14 +25,21 @@ public class JourneyController : Controller
     [HttpGet("{origin}/{destination}")]
     public async Task<ActionResult<JourneyRes>> Find(string origin, string destination, uint numberOfFlighs = 1)
     {
-        if(origin == destination)
+        try
+        {
+            if(origin == destination)
             return BadRequest("Origin and destination can't be the same");
 
-        JourneyRes? journey = await journeyBusinessLogic.GetCheapestFlightAsync(origin, destination, numberOfFlighs);
+            JourneyRes? journey = await journeyBusinessLogic.GetCheapestFlightAsync(origin, destination, numberOfFlighs);
 
-        if(journey is null)
-            return NoContent();
-        else
-            return Ok(journey);
+            if(journey is null)
+                return NoContent();
+            else
+                return Ok(journey);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
     }
 }
